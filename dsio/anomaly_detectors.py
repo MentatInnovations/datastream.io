a6099fe77df7a0bf34b390f5f1e61ca217fa7936
Christoforos Anagnostopoulos <canagnos@ment.at>
2017-12-27 01:54:58 +0000

    Fixed bugs in anomaly detectors. Example.py now shows them to be 100% accurate on toy example.

--- dsio/anomaly_detectors.py
+++ dsio/anomaly_detectors.py
@@ -6,11 +6,23 @@ import pandas as pd
 import numpy as np
 
 from scipy.stats import percentileofscore
+from scipy.stats import norm
 
 from collections import namedtuple
 
 from dsio.update_formulae import update_effective_sample_size
 from dsio.update_formulae import convex_combination, rolling_window_update, decision_rule
+
+
+def compute_confusion_matrix(detector_output, index_anomalies):
+
+    index_detected = set(np.where(detector_output)[0])
+    index_true = set(index_anomalies)
+    true_anomalies = index_detected.intersection(index_true)
+    false_anomalies = index_detected.difference(index_true)
+    return {
+        'TPR': len(true_anomalies)/(1.0*len(index_anomalies)),
+        'FPR': len(false_anomalies)/(1.0*len(detector_output))}
 
 
 class AnomalyDetector(object):
@@ -19,19 +31,12 @@ class AnomalyDetector(object):
     """
 
     @abc.abstractmethod
-    def __init__(self, variable_types=None, model_params=None, tuning_params=None, threshold=0.99):
-        self.variable_types = variable_types
-        self.model_params = model_params
-        self.tuning_params = tuning_params
-        self.threshold = threshold
-
-    @abc.abstractmethod
     def update(self, x):
         raise NotImplementedError
 
     @abc.abstractmethod
     def detect(self, x):
-        return decision_rule(self.score, self.threshold)
+        raise NotImplementedError
 
     @abc.abstractmethod
     def train(self, x):
@@ -41,26 +46,29 @@ class AnomalyDetector(object):
     def score(self, x):
         raise NotImplementedError
 
-    @abc.abstractmethod
-    def copy(self):
-        raise NotImplementedError
+    #@abc.abstractmethod
+    #def copy(self):
+    #    raise NotImplementedError
 
-    @abc.abstractmethod
-    def serialise(self):
-        raise NotImplementedError
+    #@abc.abstractmethod
+    #def serialise(self):
+    #    raise NotImplementedError
 
 
 class Gaussian1D(AnomalyDetector):
     def __init__(
             self,
             variable_types={np.dtype('float64'), np.dtype('int64'), np.dtype('int8')},
-            model_params=namedtuple('model_params', 'mu ess')(0, 0),
-            tuning_params=namedtuple('tuning_params', ['ff'])(0.9)):
+            model_params=namedtuple('model_params', 'mu std ess')(0, 1.0, 0),
+            tuning_params=namedtuple('tuning_params', ['ff'])(0.9),
+            threshold=0.99
+    ):
 
         # TODO: we must find a way to validate incoming dtypes
         self.variable_types = variable_types
         self.model_params = model_params
         self.tuning_params = tuning_params
+        self.threshold = threshold
 
     def update(self, x): # allows mini-batch
         x = pd.Series(x)
@@ -74,19 +82,25 @@ class Gaussian1D(AnomalyDetector):
             np.mean(x),
             weight=weight
         )
+        std = np.std(x)
         # update model state at the end
-        self.model_params = namedtuple('model_params', 'mu ess')(mu, ess)
+        self.model_params = namedtuple('model_params', 'mu std ess')(mu, std, ess)
 
     def train(self, x):
         x = pd.Series(x)
         assert x.dtypes in self.variable_types
         mu = np.mean(x)
+        std = np.std(x, ddof=1)
         ess = len(x)
-        self.model_params = namedtuple('model_params', 'mu ess')(mu, ess)
+        self.model_params = namedtuple('model_params', 'mu std ess')(mu, std, ess)
 
     def score(self, x):
         x = pd.Series(x)
-        return np.abs(x - self.model_params.mu)
+        scaled_x = np.abs(x - self.model_params.mu)/(1.0*self.model_params.std)
+        return norm.cdf(scaled_x)
+
+    def detect(self, x):
+        return decision_rule(self.score(x), self.threshold)
 
 
 class Percentile1D(AnomalyDetector):
@@ -94,13 +108,16 @@ class Percentile1D(AnomalyDetector):
     def __init__(
             self,
             variable_types={np.dtype('float64'), np.dtype('int64'), np.dtype('int8')},
-            model_params=namedtuple('model_params', ['sample'])([0]),
-            tuning_params=namedtuple('tuning_params', ['ff', 'w'])(1.0, 100.0)):
+            model_params=namedtuple('model_params', 'sample')([0]),
+            tuning_params=namedtuple('tuning_params', ['ff', 'w'])(1.0, 300.0),
+            threshold=0.99
+    ):
 
         # TODO: we must find a better way to validate incoming dtypes
         self.variable_types = variable_types
         self.model_params = model_params
         self.tuning_params = tuning_params
+        self.threshold = threshold
 
     def update(self, x): # allows mini-batch
         x = pd.Series(x)
@@ -117,6 +134,8 @@ class Percentile1D(AnomalyDetector):
 
     def score(self, x):
         x = pd.Series(x)
-        scores = [0.01*percentileofscore(self.model_params.sample, z) for z in x]
+        scores = pd.Series([0.01*percentileofscore(self.model_params.sample, z) for z in x])
         return scores
 
+    def detect(self, x):
+        return decision_rule(self.score(x), self.threshold)
--- examples/example.py
+++ examples/example.py
@@ -1,14 +1,21 @@
-from dsio.anomaly_detectors import Gaussian1D, Percentile1D
+import numpy as np
+from dsio.anomaly_detectors import Gaussian1D, Percentile1D, compute_confusion_matrix
 from dsio.generate_data import gen_data_with_obvious_anomalies
 
-x = gen_data_with_obvious_anomalies(n=1000, anomalies=10)
+x, index_anomalies = gen_data_with_obvious_anomalies(n=1000, anomalies=50)
 
 detector1 = Gaussian1D()
-detector1.train(x[:100])
-detector1.update(x[101:200])
-detector1.score(x)
+detector1.train(x[:50])
+detector_output1 = detector1.detect(x)
+print(compute_confusion_matrix(detector_output1, index_anomalies))
+detector1.update(x[101:])
+detector_output1 = detector1.detect(x)
+print(compute_confusion_matrix(detector_output1, index_anomalies))
 
 detector2 = Percentile1D()
-detector2.train(x[:100])
-detector2.update(x[101:200])
-detector2.score(x)
+detector2.train(x[:50])
+detector_output2 = detector2.detect(x)
+print(compute_confusion_matrix(detector_output2, index_anomalies))
+detector2.update(x[101:])
+detector_output2 = detector2.detect(x)
+print(compute_confusion_matrix(detector_output2, index_anomalies))
