b3c8e446cb0a95fa5945f677f9d825e93404050b
Christoforos Anagnostopoulos <canagnos@ment.at>
2018-01-25 16:34:43 +0200

    Added one more example

--- examples/example_cardata.py
+++ examples/example_cardata.py
@@ -1,7 +1,10 @@
 from dsio.anomaly_detectors import Gaussian1D, Percentile1D, compute_confusion_matrix
+from dsio.lof_anomaly_detector import LOFAnomalyDetector
 from dsio.generate_data import gen_data_with_obvious_anomalies
 
 x, index_anomalies = gen_data_with_obvious_anomalies(n=1000, anomalies=50)
+
+
 
 detector1 = Gaussian1D()
 detector1.fit(x[:50])
@@ -18,3 +21,12 @@ print(compute_confusion_matrix(detector_output2, index_anomalies))
 detector2.update(x[101:])
 detector_output2 = detector2.flag_anomaly(x)
 print(compute_confusion_matrix(detector_output2, index_anomalies))
+
+
+detector3 = LOFAnomalyDetector()
+detector3.fit(x[:50])
+detector_output3 = detector3.flag_anomaly(x)
+print(compute_confusion_matrix(detector_output3, index_anomalies))
+detector3.fit(x[101:])
+detector_output3 = detector3.flag_anomaly(x)
+print(compute_confusion_matrix(detector_output3, index_anomalies))
